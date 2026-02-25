b3bb55af909fcb8823679b9361f6cc92f7c32aec
Ali <39883081+goorekt@users.noreply.github.com>
2026-02-25 21:22:46 +0100

    adsf

--- NasaDem.py
+++ NasaDem.py
@@ -1,9 +1,9 @@
-
-
 import rasterio
 from rasterio.warp import transform
 
-vrt_path = "C:/Users/karlj/Desktop/nasadem_all/nasadem.vrt"
+vrt_path = "G:\\data\\zipData\\nasadem_all\\nasadem.vrt"
+
+
 def query(vrt_path, points_lonlat):
     with rasterio.open(vrt_path) as src:
         # Reproject input lon/lat -> dataset CRS if needed
@@ -17,3 +17,9 @@ def query(vrt_path, points_lonlat):
         # Sample band 1
         vals = [v[0] for v in src.sample(pts)]
         return vals
+
+
+query_points = [(-120.0, 35.0), (-121.0, 36.0)]
+elevations = query(vrt_path, query_points)
+for pt, elev in zip(query_points, elevations):
+    print(f"Elevation at {pt}: {elev} meters")
